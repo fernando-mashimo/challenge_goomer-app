81b36a5369e1e14ce624f5544685fe454f69defb
Fernando Mashimo <fernando.mashimo@yahoo.com.br>
2024-02-04 11:30:04 -0300

    refactor: redefined initial set of tables

--- prisma/migrations/20240204142846_create_tables_initial_set/migration.sql
+++ prisma/migrations/20240204142846_create_tables_initial_set/migration.sql
@@ -2,7 +2,7 @@
 CREATE TYPE "Category" AS ENUM ('Starter', 'Main', 'Dessert', 'Drink', 'Other');
 
 -- CreateEnum
-CREATE TYPE "SaleDays" AS ENUM ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+CREATE TYPE "Day" AS ENUM ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
 
 -- CreateTable
 CREATE TABLE "Restaurant" (
@@ -10,10 +10,6 @@ CREATE TABLE "Restaurant" (
     "name" TEXT NOT NULL,
     "address" TEXT NOT NULL,
     "photo" TEXT NOT NULL,
-    "openingHour" TEXT NOT NULL,
-    "closingHour" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Restaurant_pkey" PRIMARY KEY ("id")
 );
@@ -35,15 +31,35 @@ CREATE TABLE "RestaurantProductSale" (
     "id" TEXT NOT NULL,
     "saleDescription" TEXT NOT NULL,
     "salePrice" DOUBLE PRECISION NOT NULL,
-    "saleDays" "SaleDays" NOT NULL,
+    "saleDays" "Day" NOT NULL,
     "saleHours" TEXT NOT NULL,
     "restaurantProductId" TEXT NOT NULL,
 
     CONSTRAINT "RestaurantProductSale_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "RestaurantOpeningHours" (
+    "id" TEXT NOT NULL,
+    "day" "Day" NOT NULL,
+    "openingHour" TEXT NOT NULL,
+    "closingHour" TEXT NOT NULL,
+    "restaurantId" TEXT NOT NULL,
+
+    CONSTRAINT "RestaurantOpeningHours_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Restaurant_name_key" ON "Restaurant"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Restaurant_address_key" ON "Restaurant"("address");
+
 -- AddForeignKey
 ALTER TABLE "RestaurantProduct" ADD CONSTRAINT "RestaurantProduct_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "RestaurantProductSale" ADD CONSTRAINT "RestaurantProductSale_restaurantProductId_fkey" FOREIGN KEY ("restaurantProductId") REFERENCES "RestaurantProduct"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RestaurantOpeningHours" ADD CONSTRAINT "RestaurantOpeningHours_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
